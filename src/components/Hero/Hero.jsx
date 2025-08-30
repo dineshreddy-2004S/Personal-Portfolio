f17ed2c0efb82a5c472dc7a8c12478d30ec76832
dineshreddy-2004S <dineshreddy8658@gmail.com>
2025-08-30 17:50:09 +0530

    Fixed Some Errors

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <p>I am aspiring for Frontend Web Developer. I had good knowledge on HTML, CSS, JavaScript and React JS</p>
         <div className='hero-action'>
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-resume">My Resume</div>
         </div>
     </div>
   )
